5dbb38cac8d06cd115cb5c7bf76ce67d45e37f72
Anderson Malheiro <anderson.malheiro@aluno.fapce.edu.br>
2019-03-13 17:09:15 -0300

    Adding post component

---
+++ src/app/post/post.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { post } from 'selenium-webdriver/http';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+  @Input() post;
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
