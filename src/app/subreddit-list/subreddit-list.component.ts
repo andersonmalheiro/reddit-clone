e62843d6973698aea115cb99f3271294c90ac8b2
Anderson Malheiro <anderson.malheiro@aluno.fapce.edu.br>
2019-03-13 17:09:43 -0300

    Adding subreddit list component

---
+++ src/app/subreddit-list/subreddit-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subreddit-list',
+  templateUrl: './subreddit-list.component.html',
+  styleUrls: ['./subreddit-list.component.css']
+})
+export class SubredditListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
