7e1fc52fbd58dca233ceed4590ff830c20bdda61
Anderson Malheiro <anderson.malheiro@aluno.fapce.edu.br>
2019-03-13 17:12:20 -0300

    Updating styles and imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule }    from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PostComponent } from './post/post.component';
+import { HomeComponent } from './home/home.component';
+import { SubredditListComponent } from './subreddit-list/subreddit-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    PostComponent,
+    HomeComponent,
+    SubredditListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
