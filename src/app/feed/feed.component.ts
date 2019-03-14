f2db9e3734ce4a3c38eb4c6b908189a53303ee60
Anderson Malheiro <anderson.malheiro@aluno.fapce.edu.br>
2019-03-14 17:32:19 -0300

    Adding feed component

---
+++ src/app/feed/feed.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-feed',
+  templateUrl: './feed.component.html',
+  styleUrls: ['./feed.component.css']
+})
+export class FeedComponent implements OnInit {
+  @Input() posts : [];
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
