f3dd6355a046b0533bea26acd4e21159baaff96c
Anderson Malheiro <anderson.malheiro@aluno.fapce.edu.br>
2019-03-20 10:38:18 -0300

    Adding select post event on feed

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-feed',
@@ -7,10 +7,15 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class FeedComponent implements OnInit {
   @Input() posts : [];
+  @Output() post = new EventEmitter();
   
   constructor() { }
 
   ngOnInit() {
+  }
+
+  selectPost(post: {}): void {
+    this.post.emit(post);
   }
 
 }
