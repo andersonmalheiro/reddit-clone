b47662d1ef7d77c226759666cf736b2b1473b72a
Anderson Malheiro <anderson.malheiro@aluno.fapce.edu.br>
2019-03-21 16:24:09 -0300

    Adding comment and post models

---
+++ src/app/models/comment.ts
@@ -0,0 +1,9 @@
+export class Comment{
+    id: number
+    text: string
+    author: string
+    post: number
+    likes: number
+    dislikes: number
+    createdAt: Date
+}
---
+++ src/app/models/post.ts
@@ -0,0 +1,11 @@
+export class Post{
+    id: number
+    title: string
+    description: string
+    subreddit: number
+    author: string
+    image: string 
+    likes: number
+    dislikes: number
+    createdAt: Date
+}
