dcee4cfccdc37d560d87ba9080986577d9900489
Anderson Malheiro <anderson.malheiro@aluno.fapce.edu.br>
2019-03-21 16:26:20 -0300

    Separating error handler class

---
+++ src/app/handleError.ts
@@ -0,0 +1,12 @@
+import { Observable, of } from "rxjs";
+import { catchError, map, tap } from "rxjs/operators";
+
+export default function handleError<T>(operation = "operation", result?: T) {
+    return (error: any): Observable<T> => {
+        // TODO: send the error to remote logging infrastructure
+        console.error(error); // log to console instead
+
+        // Let the app keep running by returning an empty result.
+        return of(result as T);
+    };
+}
