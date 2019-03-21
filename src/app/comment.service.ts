1e5c1a3be0e49259770c4bcec4286d91041dbe2e
Anderson Malheiro <anderson.malheiro@aluno.fapce.edu.br>
2019-03-21 16:21:31 -0300

    Adding comment service

---
+++ src/app/comment.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable, of } from "rxjs";
+import { catchError, map, tap } from "rxjs/operators";
+import handleError from "./handleError";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+  
+  serverUrl = "https://rest-reddit.herokuapp.com/api/";
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  getComments(postId: number): Observable<[]> {
+    return this.http.get<[]>(this.serverUrl + `posts/${postId}/comments/`).pipe(
+      tap(_ => console.log("Comments fetched")),
+      catchError(handleError<[]>("Get comments"))
+    );
+  };
+}
